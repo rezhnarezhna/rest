311159a79e344404a3cfe58d22c081b80875b72c
unknown <greenzonecoc@gmail.com>
2023-03-17 18:38:29 +0300

    post method and json object dependency

--- src/main/java/rest/ElementResource.java
+++ src/main/java/rest/ElementResource.java
@@ -10,10 +10,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import java.sql.SQLException;
-import java.util.Dictionary;
-import java.util.Hashtable;
-import java.util.Map;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.QueryParam;
+import org.json.JSONObject;
 
 @Path("/elements")
 public class ElementResource {
@@ -34,8 +34,29 @@ public class ElementResource {
         Element element = elementDAO.getElement(id, size);
 
         String encodedData = Base64.getEncoder().encodeToString(element.getData());
-        
-        return "{\"data\":"+encodedData+"}";
-       
+
+        return "{\"data\":" + encodedData + "}";
+
     }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String getElementByIdAndSize(String requestBody) {
+        JSONObject jsonRequest = new JSONObject(requestBody);
+        int id = jsonRequest.getInt("id");
+        String size = jsonRequest.getString("size");
+
+        try {
+            elementDAO = new ElementDAO();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        Element element = elementDAO.getElement(id, size);
+        String encodedData = Base64.getEncoder().encodeToString(element.getData());
+
+        return encodedData;
+    }
+
 }
